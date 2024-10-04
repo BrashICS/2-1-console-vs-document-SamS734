15464aa20036166ff65be6eb21770cc8485a7cd7
Sam <samuel.sum@stu.ocsb.ca>
2024-10-04 11:03:51 -0400

    finished without dawson help at alllll!!!!

--- main.js
+++ main.js
@@ -5,6 +5,16 @@
  * What you see here is a block comment. More on that later. READ THE INSTRUCTIONS
  **/ 
 
+console.log("Apple Inc.")
+console.log("One Apple Park Way")
+console.log("Cupertino, CA")
+console.log("95014")
+console.log("+1 408-996-1010")
 
-// Feel free to delete this line when starting the task
-console.log("Hello world!")
+console.log("\n\n")
+
+console.log("Microsoft Corporation\nOne Microsoft Way\nRedmond, WA\n98052\n+1 425-882-8080")
+
+console.log("\n\n")
+
+console.log("x\to\tx\no\tx\to\nx\to\tx")
